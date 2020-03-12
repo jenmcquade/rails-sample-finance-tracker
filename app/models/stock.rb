522a7cfe0b0f0d70299acdf889df500e9b9c8b41
Jen McQuade <jenmcquade@gmail.com>
2020-03-12 12:46:39 -0700

    Add iex api key

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex[:publishable_access_key],
+      secret_token: Rails.application.credentials.iex[:secret_access_key],
+      endpoint: Rails.application.credentials.iex[:endpoint]
+    )
+    client.price(ticker_symbol)
+  end
 end
